1148a0f38d4791978425a5ca0a04108670683472
TransientCW <chris@myubercode.com>
2019-03-23 01:35:51 -0600

    working on projects section

--- js/main.js
+++ js/main.js
@@ -4,9 +4,18 @@ const intro =
   " for the U.S. Geological Survey in Golden, CO.\n\n" +
   "I am in my senior year finishing a Bachelors degree" +
   " in web development.\n" +
-  "When I am not working, coding, or working on school work,\n" +
+  "When I am not working, coding, or grinding out school work,\n" +
   " I am skydiving, camping, flying paragliders, and" +
   " enjoying the outdoors.";
+
+const projects = [
+  {
+    label: "Good Guys Glass",
+    description: "Auto and industrial glass repair/replacement",
+    url: "http://goodguysglass.com",
+    imageUrl: ""
+  }
+];
 
 let i = 0;
 
@@ -19,4 +28,10 @@ function type() {
     document.getElementById("terminal-prompt").innerHTML +=
       '<div class="prompt-box"></div>';
   }
+}
+
+(function createCards() {})();
+
+function createCard(card) {
+  const div = document.createElement("div");
 }
